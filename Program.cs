c49d0901d9189ec3192e2779d64741c79739c25f
Natalia <nataliahola@yandex.ru>
2023-01-17 22:28:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 2 до N
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+        int res = 1;
+       
+        if (res == 1)
+
+        {
+         res += 1;
+        }
+
+        while (res <= num)
+
+        {
+            Console.WriteLine(res);
+            res += 2;
+        }
